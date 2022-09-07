f552f9ccbe8c9ef503a392c3857f7640cb1f38d4
Kevin Chang <apog780212@gmail.com>
2022-09-07 22:03:08 +0800

    Update 2022-09-07T14:03:05.470Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,17 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+var bars = document.querySelector('.navbar-toggler i');
+bars.addEventListener('click', function (e) {
+  // 查詢目前所在元素位置
+  var cName = e.target; // console.log(cName.getAttribute('class'));
+  // 取得 i 標籤 class 名稱
+
+  var cNameClass = cName.getAttribute('class'); // 比對並替換 i 標籤 class 名稱
+
+  if (cNameClass === 'fa-solid fa-bars text-primary fa-xl') {
+    cName.className = 'fa-solid fa-xmark text-primary fa-xl';
+  } else {
+    cName.className = 'fa-solid fa-bars text-primary fa-xl';
+  }
+}, false);
 //# sourceMappingURL=all.js.map
