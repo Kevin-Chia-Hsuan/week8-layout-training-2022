ddc71c5e84e6c8e3afdf12e876cf004f7f8d3e43
Kevin Chang <apog780212@gmail.com>
2022-09-12 12:09:30 +0800

    Update 2022-09-12T04:09:26.874Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+$('.row').imagesLoaded().progress(function () {
+  $('.row').masonry(); // 渲染整體畫面
+});
 var bars = document.querySelector('.navbar-toggler i');
 bars.addEventListener('click', function (e) {
   // 查詢目前所在元素位置
