6cc668859b19628845e326fd4371af64287164b7
Kevin Chang <apog780212@gmail.com>
2022-09-13 19:51:27 +0800

    Update 2022-09-13T11:51:24.346Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -72,7 +72,7 @@ initSwiper(); // 確保圖片都載入後，以免造成剛進入頁面時圖片
 // const rowMasonry = document.querySelector('.rowMasonry');
 // console.log(rowMasonry);
 
-$('.rowMasonry').imagesLoaded().progress(function () {
-  $('.rowMasonry').masonry(); // 渲染整體畫面
+$('.row').imagesLoaded().progress(function () {
+  $('.row').masonry(); // 渲染整體畫面
 });
 //# sourceMappingURL=all.js.map
